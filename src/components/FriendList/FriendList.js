903b1406ce3cf22dddfa3a78dabe46ac663c6978
VorobyovaAnna <anyasonn1k@gmail.com>
2022-01-15 21:44:25 +0200

    add components without styless

---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import FriendListItem from "./FriendListItem";
+
+function FriendList({ friends }) {
+  return (
+    <ul className="friend-list">
+      {friends.map((friend) => (
+        <FriendListItem
+          key={friend.id}
+          avatar={friend.avatar}
+          name={friend.name}
+          isOnline={friend.isOnline}
+        />
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
+
+export default FriendList;
